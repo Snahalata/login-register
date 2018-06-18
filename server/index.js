de57a848b7d76abf70a4471d1764c6fe09299470
Snahalata <37218579+Snahalata@users.noreply.github.com>
2018-06-18 13:01:57 -0400

    Completed the password encryption

---
+++ server/index.js
@@ -0,0 +1,27 @@
+//system libraries
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//take the system port or take 3000 if not available
+const port = process.env.port || 3000;
+
+//declaring the path for user routes
+const user = require('./controller/user');
+
+//initializing the node server
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+//redirecting to the required routes
+app.use('/user',user);
+
+
+//server is listening on port 
+app.listen(port,()=>{
+    console.log("server has started on port "+port);
+})
+
